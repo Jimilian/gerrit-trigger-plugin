fe6000dca7fa0e7d40e90a1fa2c1fdaa1f39897d
Alex <mr.akbashev@gmail.com>
2017-07-25 08:51:52 +0200

    Provide the data about parent builds as environment variable to the dependent build
    - Use GerritDependencyAction to follow SECURITY-170
    - Use `Actionable.replaceAction`, because `addOrReplace` is added only
    in 2.29
    - Do not use `getExternalizableId` to provide the data about parents
    even for obsolete parents

--- src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/dependency/DependencyQueueTaskDispatcher.java
+++ src/main/java/com/sonyericsson/hudson/plugins/gerrit/trigger/dependency/DependencyQueueTaskDispatcher.java
@@ -188,9 +188,34 @@ public final class DependencyQueueTaskDispatcher extends QueueTaskDispatcher
             return new BecauseDependentBuildIsBuilding(blockingProjects);
         } else {
             logger.info("No active dependencies on project: {} , it will now build", p);
+
+            ToGerritRunListener toGerritRunListener = ToGerritRunListener.getInstance();
+            if (toGerritRunListener != null) {
+                List<Run> parentRuns = toGerritRunListener.getRuns(event);
+
+                if (parentRuns == null) {
+                    logger.info("All dependencies on project: {}, are triggered in silent mode. "
+                            + "Can not get list of actual dependencies", p);
+                    return null;
+                }
+
+                List<Run> actualDependencies = new ArrayList<Run>(dependencies.size());
+                for (Run run : parentRuns) {
+                    if (dependencies.contains(run.getParent())) {
+                        actualDependencies.add(run);
+                    }
+                }
+
+                // TODO: returning `null` from a QueueTaskDispatcher does not mean the build will start immediately.
+                // So, this line can be called multiple times. In case of performance/stability issues it makes sense to
+                // mode to TransientActionFactory or maybe a QueueListener would be better.
+                item.replaceAction(new GerritDependencyAction(actualDependencies));
+            }
+
             return null;
         }
     }
+
 
     /**
      * Gets the subset of projects which have a building element needing to complete for the same event.
@@ -199,7 +224,7 @@ public final class DependencyQueueTaskDispatcher extends QueueTaskDispatcher
      * @return the sublist of dependencies which need to be completed before this event is resolved.
      */
     protected List<Job> getBlockingDependencyProjects(List<Job> dependencies,
-            GerritTriggeredEvent event) {
+                                                    GerritTriggeredEvent event) {
         List<Job> blockingProjects = new ArrayList<Job>();
         ToGerritRunListener toGerritRunListener = ToGerritRunListener.getInstance();
         if (toGerritRunListener != null) {
